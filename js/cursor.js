34497feccfd3910d65c23e149e24133d32896886
Jorge Rodríguez <rodriguez.jorged@gmail.com>
2018-12-14 15:15:24 -0500

    Subida de Archivos

---
+++ js/cursor.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 50){
+            $('#scrollTop').fadeIn();
+            } else {
+                $('#scrollTop').fadeOut();    
+            }
+    });
+
+    $('#scrollTop').click(function(){
+        $('html,body').animate({
+            scrollTop:0
+        },1200)
+    });
+});
